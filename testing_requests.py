b1e4b7304c2771d544a34a372e00fbd9502c5e66
Rafael Araujo Lehmkuhl <rafael.lehmkuhl93@gmail.com>
2020-08-21 10:33:14 -0300

    Add post request body

--- testing_requests.py
+++ testing_requests.py
@@ -11,7 +11,23 @@ response_get = requests.get(get_endpoint, headers=headers)
 
 leafUserId = response_get.json()[0].get('leafUserId')
 post_endpoint = f'{base_url}/users/{leafUserId}/fields/'
-response_post = requests.post(post_endpoint, headers=headers)
+post_request_body = {
+    "geometry": {
+        "type": "MultiPolygon",
+        "coordinates": [
+            [
+                [
+                    [-93.48821327980518, 41.77137549568163],
+                    [-93.48817333680519, 41.77143534378164],
+                    [-93.48821327390516, 41.76068857977987],
+                    [-93.48821327980518, 41.77137549568163]
+                ]
+            ]
+        ]
+    }
+}
+response_post = requests.post(
+    post_endpoint, headers=headers, json=post_request_body)
 
 print('\n GET REQUEST: \n')
 pprint(response_get.json())
