b78e704a7dcb00094196ea5fbfb00970c1497a99
Rafael Araujo Lehmkuhl <rafael.lehmkuhl93@gmail.com>
2020-08-21 10:12:10 -0300

    Add (wrong) post request

--- testing_requests.py
+++ testing_requests.py
@@ -3,9 +3,20 @@ import os
 from pprint import pprint
 
 TOKEN = os.environ['LEAF_TOKEN']
-
-endpoint = 'https://api.withleaf.io/services/fields/api/fields'
 headers = {'Authorization': f'Bearer {TOKEN}'}
+base_url = 'https://api.withleaf.io/services/fields/api'
 
-response = requests.get(endpoint, headers=headers)
-pprint(response.json())
+get_endpoint = f'{base_url}/fields'
+response_get = requests.get(get_endpoint, headers=headers)
+
+leafUserId = response_get.json()[0].get('leafUserId')
+post_endpoint = f'{base_url}/users/{leafUserId}/fields/'
+response_post = requests.post(get_endpoint, headers=headers)
+
+print('\n GET REQUEST: \n')
+pprint(response_get.json())
+print('\n ---------------------------------------------------------------- \n')
+
+print('\n POST REQUEST: \n')
+pprint(response_post.json())
+print('\n ---------------------------------------------------------------- \n')
